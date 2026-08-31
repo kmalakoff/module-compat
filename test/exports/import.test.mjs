d5aee28f1c2a2d2f6dcc8c82d6924df9dbda9c27
Kevin Malakoff <kmalakoff@gmail.com>
2026-08-31 07:24:54 -0700

    add export tests for module functions

---
+++ test/exports/import.test.mjs
@@ -0,0 +1,23 @@
+import assert from 'assert';
+import { extToModuleType, loadModule, loadModuleSync, moduleType, supportsESM, supportsSyncRequireESM } from 'module-compat';
+
+describe('exports .mjs', () => {
+  it('extToModuleType', () => {
+    assert.equal(typeof extToModuleType, 'function');
+  });
+  it('loadModule', () => {
+    assert.equal(typeof loadModule, 'function');
+  });
+  it('loadModuleSync', () => {
+    assert.equal(typeof loadModuleSync, 'function');
+  });
+  it('moduleType', () => {
+    assert.equal(typeof moduleType, 'function');
+  });
+  it('supportsESM', () => {
+    assert.equal(typeof supportsESM, 'function');
+  });
+  it('supportsSyncRequireESM', () => {
+    assert.equal(typeof supportsSyncRequireESM, 'function');
+  });
+});
